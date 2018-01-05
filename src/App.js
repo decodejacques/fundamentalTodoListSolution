b53bebb56f0439795550616eded1e9de560cb19b
jacques@decodemtl.com <jacques@decodemtl.com>
2018-01-05 11:42:52 -0500

    fifth step

--- src/App.js
+++ src/App.js
@@ -8,18 +8,21 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 class App extends Component {
   constructor() {
     super();
-    this.state = ['first item', 'second item']
+    this.state = { items: ['first item', 'second item'] }
   }
   displayItem = (item) => {
     return (<li> {item} </li>)
   }
-  render() {
+  addItem = (item) => {
+    this.setState(st => ({ items: st.items.concat(item) }))
+  }
 
+  render() {
     return (<div>
       <input /> <button>press me</button>
       <ul>
-        {this.state.map(this.displayItem)}
-        
+        {this.state.items.map(this.displayItem)}
+
       </ul>
     </div>)
 
