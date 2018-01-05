e6287c0fbc9a3741230f0b2160eda2fe26b8be2f
jacques@decodemtl.com <jacques@decodemtl.com>
2018-01-05 11:22:52 -0500

    first step

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+
+
+class App extends Component {
+  
+  render() {
+    return (<div> 
+      <input /> <button>press me</button>
+      <ul>
+      <li> first item </li>
+      <li> second item </li> 
+      </ul>
+    </div>)
+
+  }
+
+
+
+}
+
+export default App;
